f295fa31fbb9ec5ef4f66042188de83e471c099a
Ryan Wang <ryanwang601@gmail.com>
2024-07-31 03:46:49 -0400

    mobile adjustments

---
+++ script.js
@@ -0,0 +1,8 @@
+// function adjustBodyPadding() {
+//   const navbar = document.getElementById('navbar');
+//   const navbarHeight = navbar.offsetHeight;
+//   document.body.style.paddingTop = `${navbarHeight+2}px`;
+// }
+
+// window.onload = adjustBodyPadding;
+// window.onresize = adjustBodyPadding;
